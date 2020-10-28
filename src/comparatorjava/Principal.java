512e6e5b45dfa8f0a7a2bb1680826f9cf1b67216
caemci <carlosecimino@gmail.com>
2020-10-28 17:55:57 -0300

    Más descriptivo

--- src/comparatorjava/Principal.java
+++ src/comparatorjava/Principal.java
@@ -7,33 +7,33 @@ import java.util.Collections;
 /**
  *
  * @author caemci
+ * @see https://github.com/caemci
  */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Genero lista de alumnos
         ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
-        alumnos.add( new Alumno("Pedro","Fulano",7) );
-        alumnos.add( new Alumno("Juan","Mengano",9) );
+        alumnos.add( new Alumno("Juan","Fulano",9) );
+        alumnos.add( new Alumno("Pedro","Mengano",7) );
+        alumnos.add( new Alumno("Ana","Merez",8) );
+        alumnos.add( new Alumno("Maria","Perez",6) );
         alumnos.add( new Alumno("Luis","Sultano",8) );
                 
-        System.out.println("Listado de alumnos original:");
-        System.out.println(alumnos); // Mostrar
+        System.out.println("Listado de alumnos original");
+        UtilCollections.mostrarColeccion(alumnos); // Mostrar
         
-        System.out.println("\nListado de alumnos ordenado por nombre:");
-        Collections.sort(alumnos, Alumno.COMPARATOR_NOMBRE); // Ordenar
-        System.out.println(alumnos); // Mostrar
+        System.out.println("\nListado de alumnos ordenado por nombre");
+        Collections.sort(alumnos, Alumno.COMPARATOR_NOMBRE); // Ordena por nombre de forma ascendente
+        UtilCollections.mostrarColeccion(alumnos); // Mostrar
         
-        System.out.println("\nListado de alumnos ordenado por apellido:");
-        Collections.sort(alumnos, Alumno.COMPARATOR_APELLIDO); // Ordenar
-        System.out.println(alumnos); // Mostrar
+        System.out.println("\nListado de alumnos ordenado por apellido");
+        Collections.sort(alumnos, Alumno.COMPARATOR_APELLIDO); // Ordena por apellido de forma ascendente
+        UtilCollections.mostrarColeccion(alumnos); // Mostrar
         
-        System.out.println("\nListado de alumnos ordenado por nota:");
-        Collections.sort(alumnos, Alumno.COMPARATOR_NOTA); // Ordenar
-        System.out.println(alumnos); // Mostrar
+        System.out.println("\nListado de alumnos ordenado por nota");
+        Collections.sort(alumnos, Alumno.COMPARATOR_NOTA); // Ordena por nota de forma ascendente
+        UtilCollections.mostrarColeccion(alumnos); // Mostrar
     }
     
 }
